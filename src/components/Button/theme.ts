808492f70978016f681574681d85033de4cbf729
block-kitty <liam.mulv94@gmail.com>
2021-05-30 18:31:27 +1000

    Added gradients

---
+++ src/components/Button/theme.ts
@@ -0,0 +1,65 @@
+import { scales, variants } from "./types";
+import {baseColors} from '../../theme/colors'
+
+export const scaleVariants = {
+  [scales.MD]: {
+    height: "48px",
+    padding: "0 24px",
+  },
+  [scales.SM]: {
+    height: "32px",
+    padding: "0 16px",
+  },
+  [scales.XS]: {
+    height: "20px",
+    fontSize: "12px",
+    padding: "0 8px",
+  },
+};
+
+export const styleVariants = {
+  [variants.PRIMARY]: {
+    backgroundColor: "primary",
+    color: "white",
+    border: "2px solid",
+    borderColor: "primaryDark",
+    boxShadow: `0 .2rem 0 0 ${baseColors.primaryDark}`,
+    transition: 'all .2s cubic-bezier(.06,.67,.37,.99)',
+    ":hover": {
+      transform: 'translateY(-.2rem)',
+      boxShadow: `0 .4rem 0 0 ${baseColors.primaryDark}`,
+    }
+  },
+  [variants.SECONDARY]: {
+    backgroundColor: "transparent",
+    border: "2px solid",
+    borderColor: "primary",
+    boxShadow: "none",
+    color: "primary",
+    ":disabled": {
+      backgroundColor: "transparent",
+    },
+  },
+  [variants.TERTIARY]: {
+    backgroundColor: "tertiary",
+    boxShadow: "none",
+    color: "primary",
+  },
+  [variants.SUBTLE]: {
+    backgroundColor: "textSubtle",
+    color: "backgroundAlt",
+  },
+  [variants.DANGER]: {
+    backgroundColor: "failure",
+    color: "white",
+  },
+  [variants.SUCCESS]: {
+    backgroundColor: "success",
+    color: "white",
+  },
+  [variants.TEXT]: {
+    backgroundColor: "transparent",
+    color: "primary",
+    boxShadow: "none",
+  },
+};
